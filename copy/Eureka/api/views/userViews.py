20e66fcfcc3f4363f2850121c96e70d784843df6
l4z0-space <lazaronshyta@gmail.com>
2021-04-09 10:08:11 +0300

    rm

--- copy/Eureka/api/views/userViews.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import status, generics
-from django.contrib.auth.models import User
-from ..serializers import UserSerializer
-
-
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
